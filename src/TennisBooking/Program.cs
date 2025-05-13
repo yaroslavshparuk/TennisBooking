5adef9ef0388fc09e6b06d5d2032d129020d4fc4
yaroslavshparuk <shparuk1996@gmail.com>
2025-05-13 07:55:17 +0000

    init, working version

---
+++ src/TennisBooking/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TennisBooking.Options;
+using TennisBooking.Services;
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureServices((ctx, services) =>
+    {
+        var skeddaConfig = ctx.Configuration.GetSection("SkeddaConfig");
+        services.Configure<SkeddaOptions>(skeddaConfig);
+        services.AddHttpClient<BookingService>(client =>
+        {
+            var opts = skeddaConfig.Get<SkeddaOptions>();
+            client.BaseAddress = new Uri(opts.ApiBaseUrl);
+        });
+        services.AddHostedService<BookingService>();
+    })
+    .RunConsoleAsync();
